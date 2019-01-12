a3c271c35596b39de52b23d489cf862b5fd3eed4
lfalleri <30404166+lfalleri@users.noreply.github.com>
2019-01-12 21:07:07 +0100

    Update views.py

--- messaging/views.py
+++ messaging/views.py
@@ -49,38 +49,48 @@ class ContactEmailView(views.APIView):
     def post(self, request, format=None):
         data = json.loads(request.body)
 
-        type = data['type']
         name = data['name']
-        email= data['email']
+        email = data['email']
         tel = data['tel']
-        message =data['message']
+        message = data['message']
 
-        mail = EmailMultiAlternatives(
-            subject="Subject",
-            body="Body",
-            from_email="laurent.falleri@gmail.com",
-            to=["laurent.falleri@gmail.com"],
-            headers={"Reply-To": "laurent.falleri@gmail.com"}
-        )
+        staff_email = getEmails()
+        subject = "Demande de contact"
+        message_content = """
+            <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+            <html>
+            <head>
+            <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+            <title>Demande de contact 2</title>
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+            </head>
+            <p style="font-family : Montserrat;font-size:130%%;">
+               CafeAum,<br>
+               %s (email : %s / tel : %s) a laissé une demande de contact. Son message :<br>
+               "%s"
+               <br><br>
+               Merci de lui répondre dans les meilleurs délais.
+               <br>
+            </p>
+            </html>
+        """%(name, email, tel, '<br>'.join(message.splitlines()))
+        
+        sg = sendgrid.SendGridAPIClient(apikey="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")
 
-        # Add template (ContactMessage)
-        mail.template_id = '4f9c3e44-7d6f-4ff8-a8f2-99018e998aff'
+        from_email = Email("contact@cafe-aum.fr")
+        to_email = Email("contact@cafe-aum.fr")
+        content = Content("text/html", content)
+        mail = Mail(from_email, subject, to_email, content)
 
-        # Replace substitutions in sendgrid template
-        mail.substitutions = {'%type%': type,
-                              '%email%' : email,
-                              '%nom%': name,
-                              '%message%':message,
-                              '%tel%': tel}
+        response = sg.client.mail.send.post(request_body=mail.get())
 
-        # Add categories
-        mail.categories = [
-            'contact',
-        ]
-
-        mail.send()
-
-        return Response({
-            'status': 'OK',
-            'message': 'Email sent'
-        }, status=status.HTTP_200_OK)
+        if (response.status_code >= 200) and (response.status_code < 300):
+            return Response({
+                'status': 'OK',
+                'message': 'Email sent'
+            }, status=status.HTTP_200_OK)
+        else:
+            return Response({
+                'status': 'KO',
+                'message': 'Error'
+            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
